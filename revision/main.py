67bef32d612e1472b2dec7bd7deb81dbaf43de30
nimo22 <berry.mohamad-ali@courrier.uqam.ca>
2021-11-25 03:09:16 -0500

    day 1 (again)

--- revision/main.py
+++ revision/main.py
@@ -1,4 +1,7 @@
 from first_class_function import Fc
+from wraper import Wraper
+
+
 def adder(*numebers):
     y = 0
     for x in numebers:
@@ -11,21 +14,41 @@ def siteur(**kwargs):
         print(x)
 
 
+def wrap(func):
+    def insi():
+        print("hi")
+        return func()
+
+    return insi
+
+
+@wrap #run=wrap(run)
+def run():
+    print("runing")
+
+
+
+
 if __name__ == '__main__':
+    run()
     adder(10, 10)
-    dic={
-        "Firstname" : {"Sita":10,"dsa":123},
-        "Lastname" : "Sharma",
-        "Age" : 22,
-        "Phone" : 1234567890
+    dic = {
+        "Firstname": {"Sita": 10, "dsa": 123},
+        "Lastname": "Sharma",
+        "Age": 22,
+        "Phone": 1234567890
     }
 
     siteur(**dic)
-    x=3
-    print("log",x)
+    x = 3
+    print("log", x)
     print(f"log {x}")
     ####################################################################
-    fc=Fc()
-    func=fc.html
-    writer=func("h1")
+    fc = Fc()
+    func = fc.html
+    writer = func("h1")
     writer("hello world!")
+    ##########################################################
+    wr = Wraper()
+    x = wrap(wr.run)
+
---
+++ revision/wraper.py
@@ -0,0 +1,14 @@
+
+
+class Wraper:
+    def __init__(self):
+        pass
+
+    def wrap(func):
+        def insi():
+            return func()
+
+        return insi
+    @wrap
+    def run(self):
+        print("runing")
